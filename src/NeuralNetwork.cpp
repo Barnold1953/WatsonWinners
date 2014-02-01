4b4eeb2b98b301fb344d1459c8d6dce0697a3b01
PixelWars <apool@utk.edu>
2014-02-01 15:05:36 -0500

    Added initialize network function

--- src/NeuralNetwork.cpp
+++ src/NeuralNetwork.cpp
@@ -31,6 +31,49 @@ double NeuralNetwork::feedForward(vector <double> &data, bool truth)
 	}
 }
 
+
+
+void NeuralNetwork::initializeNetwork(string filename) {
+	ifstream inFile;
+	inFile.open(filename.c_str());
+
+	string variableName;
+	string variableValue;
+	string line;
+	bool foundEq = false;
+	while(getline(inFile,line)) { //while the file isn't all read
+		for(int i=0; i < line.size(); i++) {
+			if(line[i] != '=' && foundEq == false) { //get the equal sign
+				continue;
+			}
+			else if(line[i] == '=') { //set everything before to varibale name
+				variableName = line.substr(0,i);
+				foundEq = true;
+			}
+			else { //set everything after to variable value
+				variableValue = line.substr(i);
+				break;
+			}
+		}
+		if(variableName.compare("neuronsPerLayer") == 0) { //assign values
+			neuronsPerLayer = atoi(variableValue.c_str());
+		}
+		if(variableName.compare("layers") == 0) {
+			layers = atoi(variableValue.c_str());
+		}
+		if(variableName.compare("randomSeed") == 0) {
+			randomSeed = atoi(variableValue.c_str());
+		}
+		if(variableName.compare("initialRandomBias") == 0) {
+			double temp = atof(variableValue.c_str());
+			if(temp == -1) initialRandomBias = time(NULL); //if it's -1, do a random seed
+			else initialRandomBias = temp;
+		}
+		foundEq=false;
+	}
+	inFile.close();
+}
+
 void NeuralNetwork::trainNet(vector <vector <double> > &data, vector <bool> &truths)
 {
 	double error;
